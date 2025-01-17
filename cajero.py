6369c49145951fd5e0b7bf16112da1ba36c06869
Poloche <sebastianpolochetellez@gmail.com>
2025-01-16 20:52:44 -0500

    Update cajero.py
    Se le crearon funciones para cada opción y seguirá con más modificaciones en el código

--- cajero.py
+++ cajero.py
@@ -1,40 +1,53 @@
 # cajero automatico
 
-def cajero(clave):
-    clave = "2053"
-    saldo = 500000
+def retirar_dinero():
+    print("cuanto dinero desea retirar")
+    usuario = int(input(""))
+    resultado = saldo - usuario
+    print(f"retirando {resultado}")
+    print(f"tu saldo sobrante es {saldo}")
+
+def depositar_dinero():
+    print("cuanto dinero desea depositar")
+    usuario = int(input(""))
+    resultado = saldo + usuario
+    print(f"se deposito: {usuario} tu total es: {resultado}")
+
+def consultar_saldo():
+    print(f"tu saldo es: {saldo}")
+
+clave = 2053
+saldo = 500000
+def main():
     intentos = 0
     while True:
-        print("Ingresa tu clave")
-        usuario = input("")
+        usuario = int(input("ingresa tu clave: "))
         if usuario != clave:
             intentos += 1
-            print("clave incorrecto, vuelve a intentarlo")
+            print("clave incorrecta, vuelve a intentarlo")
             if intentos == 3:
                 return print("Muchos intentos, vuelve más tarde")
         if usuario == clave:
             break
-    print("Bienvenido al banco que desea consultar")
-    print("1) retirar dinero")
-    print("2) depositar dinero")
-    print("3) consultar saldo")
-    opcion = float(input(""))
-    if opcion == 1:
-        print(f"Tu saldo es de {saldo} pesos colombianos")
-        print("cuanto dinero desea retirar")
-        usuario = float(input(""))
-        resultado = saldo - usuario
-        print(f"retirando {usuario} pesos colombianos")
-        print(f"tu saldo sobrante es de {resultado} pesos colombianos")
-    if opcion == 2:
-        print(f"Tu saldo es de {saldo} pesos colombianos")
-        print("cuanto dinero desea depositar")
-        usuario = float(input(""))
-        resultado = saldo + usuario
-        print(f"depositando {usuario} pesos colombianos")
-        print(f"tu saldo total es de {resultado} pesos colombianos")        
-    if opcion == 3:
-        print("consultar saldo")
-        print(f"tu saldo total es de {saldo} pesos colombianos")
 
-cajero(clave="")
+    while usuario != 4:
+        print("\n Bienvenido al banco que desea consultar")
+        print("1) retirar dinero")
+        print("2) depositar dinero")
+        print("3) consultar saldo")
+        print("4) salir")
+
+        usuario = int(input("que desea: "))
+        if usuario == 1:
+            retirar_dinero()
+        elif usuario == 2:
+            depositar_dinero()
+        elif usuario == 3:
+            consultar_saldo()
+        elif usuario == 4:
+            print("hasta luego")
+        else:
+            print("opcion invalida")
+
+if __name__ == "__main__":
+    main()
